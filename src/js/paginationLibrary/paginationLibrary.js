6cd0c6eee740dd1526693bbac2fbf0991abac96f
Illia Kazmin <kazmin1c@gmail.com>
2022-10-12 15:41:18 +0300

    made chenches in padinationLibrary

--- src/js/paginationLibrary/paginationLibrary.js
+++ src/js/paginationLibrary/paginationLibrary.js
@@ -149,7 +149,7 @@ export default class PaginationLibrary {
                 // this.currentPage = Number(currentPage);
                 this.clearPaginationLibrary();
                 const ul = document.createElement("ul");
-                ul.classList.add("pagination-list");
+                ul.classList.add("paginationLibrary-list");
                 // console.log("totalElements", totalElements);
                 // console.log("PerPage", perPage);
                 const totalOfBtn = Math.ceil(this.totalElements / this.perPage);
@@ -196,17 +196,25 @@ export default class PaginationLibrary {
                 let isArrowRight = "";
                 const li = document.createElement("li");
                 if (item === "arrow-left") {
-                        var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
                         svg.setAttribute("width", 16);
                         svg.setAttribute("heigth", 16);
                         svg.setAttributeNS(null, "viewBox", "0 0 32 32");
-                        svg.innerHTML = `<use href="${iconsSVG}#icon-arrow-left"></use>`
+                        svg.innerHTML = `<use href="${iconsSVG}#icon-arrow-left"></use>`;
+                        svg.classList.add("paginationLibrary-list__icon");
                         txt = svg;
                         // txt = document.createTextNode("<-");
                         item = this.currentPage - 1;
                         isArrowLeft = true;
                 } else if (item === "arrow-right") {
-                        txt = document.createTextNode("->");
+                        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                        svg.setAttribute("width", 16);
+                        svg.setAttribute("heigth", 16);
+                        svg.setAttributeNS(null, "viewBox", "0 0 32 32");
+                        svg.innerHTML = `<use href="${iconsSVG}#icon-arrow-right"></use>`;
+                        svg.classList.add("paginationLibrary-list__icon");
+                        txt = svg;
+                        // txt = document.createTextNode("->");
                         item = this.currentPage + 1;
                         isArrowRight = true;
                 } else {
@@ -215,6 +223,9 @@ export default class PaginationLibrary {
                 // console.log(txt);
                 li.appendChild(txt);
                 li.classList.add("paginationLibrary-list__item");
+                if (isArrowRight || isArrowLeft) {
+                        li.classList.add("paginationLibrary-list__item--arrow");
+                }
                 if (
                         item === "..." ||
                         (isArrowLeft === true && this.currentPage === 1) ||
