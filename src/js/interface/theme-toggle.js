b1d234e54e53957272e4b83078a199feecc90e7e
DanStelmakh <danylo.stelmakh@gmail.com>
2022-10-16 22:50:08 +0300

    slider theme

--- src/js/interface/theme-toggle.js
+++ src/js/interface/theme-toggle.js
@@ -1,7 +1,7 @@
 const themeToggleBtn = document.querySelector(".theme-toggle__wrapper");
 
-if (localStorage.getItem("dark-theme")) {
-        document.querySelector("body").classList.toggle("dark-theme");
+if (!localStorage.getItem("dark-theme")) {
+        document.querySelector("body").classList.add("dark-theme");
 }
 
 themeToggleBtn.addEventListener("click", onThemeToggleBtnClick);
@@ -10,7 +10,6 @@ function onThemeToggleBtnClick() {
         localStorage.removeItem("dark-theme");
 
         const themeBtn = document.querySelector(".theme-toggle__thumb");
-        //   themeBtn.style.backgroundColor = "teal";
         themeBtn.classList.toggle(".theme-toggle__thumb");
         themeBtn.classList.toggle("theme-toggle__thumb--after");
 
